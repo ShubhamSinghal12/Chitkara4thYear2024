a643a2dfd5f71fc60a202aba5f647835408f3aa9
Shubham Singhal <shubhamsinghal12@gmail.com>
2024-07-13 09:15:47 +0530

    Week3

---
+++ G4/Lec11/CountPalindromicSS2.java
@@ -0,0 +1,46 @@
+package Lec11;
+
+public class CountPalindromicSS2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public int oddPss(String s)
+	{
+		int count = 0;
+		for(int i = 0; i < s.length(); i++)
+		{
+			count++;
+			int l = i-1;
+			int r = i+1;
+			while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r))
+			{
+				count++;
+				l--;
+				r++;
+			}
+		}
+		return count;
+	}
+	
+	public int evenPss(String s)
+	{
+		int count = 0;
+		for(int i = 0; i < s.length()-1; i++)
+		{
+			int l = i;
+			int r = i+1;
+			while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r))
+			{
+				count++;
+				l--;
+				r++;
+			}
+		}
+		return count;
+	}
+
+}
---
+++ G5/Lec10/GroupAnagram.java
@@ -0,0 +1,24 @@
+package Lec10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String st = "safhahfgadgh";
+		char[] cha = st.toCharArray();
+		Arrays.sort(cha);
+		String sst = String.valueOf(cha);
+		System.out.println(sst);
+		
+		
+		
+		
+		
+	}
+
+}
